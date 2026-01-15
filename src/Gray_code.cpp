453166c0fb9ef49eb00a40293f555c82e6d2cf46
SLKryim <623962644@qq.com>
2026-01-15 11:03:27 +0800

    feat:add step1,transfer port value into gray code

--- src/Gray_code.cpp
+++ src/Gray_code.cpp
@@ -14,12 +14,48 @@
 using namespace std;
 
 
+uint16_t binary_to_gray(uint16_t x) {
+    return x ^ (x >> 1);
+}
+
+
+void Port_to_Gray(const vector<PortRule> &port_table, vector<GrayCodedPort> &gray_coded_ports)
+{
+    for (const auto &pr : port_table) {
+        GrayCodedPort gcp;
+
+        // Gray code conversion for source port range
+        gcp.src_port_lo_gray = binary_to_gray(pr.src_port_lo);
+        gcp.src_port_hi_gray = binary_to_gray(pr.src_port_hi);
+
+        // Gray code conversion for destination port range
+        gcp.dst_port_lo_gray = binary_to_gray(pr.dst_port_lo);
+        gcp.dst_port_hi_gray = binary_to_gray(pr.dst_port_hi);
+
+        // Copy other fields
+        gcp.priority = pr.priority;
+        gcp.action = pr.action;
+
+        gray_coded_ports.push_back(gcp);
+    }
+}
+
 auto Port_Gray_coding(const vector<PortRule> &port_table) -> vector<GrayCodedPort>
 {
     vector<GrayCodedPort> gray_coded_ports;
 
-    
+    //step 1: Gray coding
+    Port_to_Gray(port_table, gray_coded_ports);
+
+    //step 2: Merge ranges for shorter TCAM
 
 
     return gray_coded_ports;
 }
+
+
+#ifdef DEMO_LOADER_MAIN
+int main(int argc, char **argv) {
+    return 0;
+}
+#endif
--- src/Gray_code.hpp
+++ src/Gray_code.hpp
@@ -7,8 +7,20 @@
 // ---------------Struct Declarations---------------------
 struct GrayCodedPort
 {
+    uint16_t src_port_lo_gray;  //src s
+    uint16_t src_port_hi_gray;  //src e
+    uint16_t dst_port_lo_gray;  //dst s
+    uint16_t dst_port_hi_gray;  //dst e
+    uint16_t P;                 // least common ancestor of s and e;
+    uint32_t priority;          // Rule priority
+    std::string action;         // Action string
+};
+
+struct GrayTCAM_Entry
+{
     /* data */
 };
+
 
 
 
